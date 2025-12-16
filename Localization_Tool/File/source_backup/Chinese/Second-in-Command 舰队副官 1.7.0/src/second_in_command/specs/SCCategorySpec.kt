4270874cbb0ed28a9f10832b7fd9363647cd431a
RoyEngine <49112231+RoyEngine@users.noreply.github.com>
2025-12-17 01:05:10 +0800

    Remove ignored files from git

--- Localization_Tool/File/source_backup/Chinese/Second-in-Command 舰队副官 1.7.0/src/second_in_command/specs/SCCategorySpec.kt
+++
@@ -1,9 +0,0 @@
-package second_in_command.specs
-
-import java.awt.Color
-
-data class SCCategorySpec(
-    var id: String,
-    var name: String,
-    var color: Color,
-)
--- Localization_Tool/File/source_backup/English/Quality Captains 1.7.0/src/dal/plugins/Captains_Lunasettings.java
+++
@@ -1,9 +0,0 @@
-package dal.plugins;
-
-import lunalib.lunaSettings.LunaSettings;
-
-public class Captains_Lunasettings {
-	public static void init() {
-		LunaSettings.addSettingsListener(new qcDynamicSettings());
-	}
-}
